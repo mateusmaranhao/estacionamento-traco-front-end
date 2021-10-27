a88e04ca0ca54770bd8cc3cda43d400753145257
Mateus <mateusmaranhao0210@gmail.com>
2021-10-27 15:00:36 -0300

    feat: pegar dados do formulário

---
+++ controller/cadastro.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('signup-form')
+form.addEventListener('submit', function(event) {
+  event.preventDefault()
+
+  const clientSingup = {
+    model: document.getElementById('model').value,
+    label: document.getElementById('label').value,
+    type: document.getElementById('type').value,
+    owner: document.getElementById('name').value,
+    observation: document.getElementById('observation').value
+  }
+
+  console.log(clientSingup)
+})
