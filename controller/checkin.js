57cfba6f66637a1e51be2126119f673a7b49f7ae
Mateus <mateusmaranhao0210@gmail.com>
2021-11-01 08:05:36 -0300

    feat: mostrar as placas dos carros

--- controller/checkin.js
+++ controller/checkin.js
@@ -3,8 +3,25 @@ import { view } from "../view/index.js"
 
 view.getCheckinHTML();
 
+let idCheckin = [];
 service.getActivities().then((dados) => {
   dados.forEach(element => {
-    
-  })
-})
+    if(element != null) {
+      idCheckin.push(element.vehicles_id)
+    }
+  });
+  getVehicle()
+});
+
+const getVehicle = () => {
+  service.getVehicle().then((dados) => {
+    dados.forEach((element) => {
+      if(idCheckin.includes(element.id)) {
+        console.log(element)
+      }
+      if(element.label != null) {
+        console.log(element)
+      }
+    })
+  });
+}
