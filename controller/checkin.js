b94fb08542457be1146ae76f412ff4a6baeb8a95
Mateus <mateusmaranhao0210@gmail.com>
2021-11-01 08:10:38 -0300

    feat: criar função para adicionar linhas na tabela de check-in

--- controller/checkin.js
+++ controller/checkin.js
@@ -17,11 +17,26 @@ const getVehicle = () => {
   service.getVehicle().then((dados) => {
     dados.forEach((element) => {
       if(idCheckin.includes(element.id)) {
-        console.log(element)
+        createNewLine(element)
       }
       if(element.label != null) {
         console.log(element)
       }
     })
   });
+}
+
+const createNewLine = (object) => {
+  const table = document.getElementById('tbody');
+  const newLine = document.createElement('tr')
+
+  const dadosHTML = `
+    <td>${object.model}</td>
+    <td>${object.label}</td>
+    <td>
+        <a id="${object.id}" class="btn-link">Check-out</a>
+    </td>
+  `
+  newLine.innerHTML = dadosHTML;
+  return table.appendChild(newLine);
 }
