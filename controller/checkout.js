0b1f49ecb118c43cb113593c9584b78a057350f3
Mateus <mateusmaranhao0210@gmail.com>
2021-11-02 09:11:00 -0300

    fix: criar função que calcula quanto tempo o veículo ficou no estacionamento

--- controller/checkout.js
+++ controller/checkout.js
@@ -38,5 +38,14 @@ const addParamsInput = (element) => {
   const checkin = new Date(element.checkin_at);
   const checkout = new Date();
   const time = checkout - checkin;
-  console.log(new Date(time))
+  const hour = calcHour(time);
+  console.log(hour)
+}
+
+const calcHour = (timeMS) => {
+  const time = {
+    horas: +(timeMS / 3600000).toFixed(0),
+    minutos: +((timeMS / 60000) % 60).toFixed(0)
+  }
+  return time;
 }
