cbeff27a36181ddbec252639dd19bda8c98a9ed3
Mateus <mateusmaranhao0210@gmail.com>
2021-11-02 09:24:57 -0300

    feat: formatar o modo em que os valores são exibidos

--- controller/checkout.js
+++ controller/checkout.js
@@ -34,7 +34,7 @@ const searchRegister = (id) => {
   })
 }
 
-const hourValue = 2;
+const hourValue = 3;
 const minuteValue = hourValue / 60;
 
 const addParamsInput = (element) => {
@@ -46,6 +46,22 @@ const addParamsInput = (element) => {
   console.log(totalPrice)
   const hourInput = document.getElementById('totalHora')
   const totalInput = document.getElementById('totalPagar')
+
+  // Mostrar hora
+  if(hour.horas < 10 &&  hour.minutos < 10) {
+    hourInput.value = `Tempo 0${hour.horas}h0${hour.minutos}min`;
+  } else if(hour.horas < 10 &&  hour.minutos > 10) {    
+    hourInput.value = `Tempo 0${hour.horas}h${hour.minutos}min`;
+  } else {    
+    hourInput.value = `Tempo ${hour.horas}h${hour.minutos}min`; 
+  }
+
+  // Mostrar valor
+  if(totalPrice < 10) {
+    totalInput.value = `R$ 0${totalPrice.toFixed(2)}`;
+  } else{
+    totalInput.value = `R$ ${totalPrice.toFixed(2)}`;
+  }
 }
 
 const calcHour = (timeMS) => {
