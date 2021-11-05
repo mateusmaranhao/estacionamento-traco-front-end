457ab6877c2210993a31f07de8a48e0610e15b89
Mateus <mateusmaranhao0210@gmail.com>
2021-11-05 08:37:19 -0300

    feat: adicionar um gráfico de exemplo no faturamento

--- controller/faturamento.js
+++ controller/faturamento.js
@@ -97,4 +97,25 @@ export const billingComponent = () => {
     document.getElementById('qtd').innerText = countObject.i;
     document.getElementById('total').innerText = `R$ ${countObject.total.toFixed(2)}`;
   }
+
+  google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
 }
